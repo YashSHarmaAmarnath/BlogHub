a9557563fe1ff39693def86f4167e033c19e8f42
Yash <yashasharma21005@gmail.com>
2024-03-16 01:53:35 +0530

    UI changed,serach implemented

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,jsonify
 from flask_pymongo import PyMongo
+from bson import ObjectId  
 
 logined = False
 currentuser  = None
@@ -18,22 +19,18 @@ def index():
 
 @app.route("/create", methods=['GET', 'POST'])
 def create():
+    if not logined:
+        return redirect('/login')
+        
     if request.method == 'POST':
         # Handle form submission
         title = request.form['title']
         content = request.form['content']
         author = request.form['author']
-        image = request.files['image']
-        
-        # Save the image to the filesystem
-        try:
-            image.save(image.filename)
-        except:
-            pass
-        
+                
         # Insert blog post data into MongoDB
         collection = mongo.db.blogs
-        collection.insert_one({'title': title, 'content': content, 'author': author, 'image': image.filename})
+        collection.insert_one({'title': title, 'content': content, 'author': author})
         
         # Redirect to the blogs page
         return redirect(url_for('all_blogs'))
@@ -42,6 +39,9 @@ def create():
 
 @app.route("/blogs")
 def all_blogs():
+    if not logined:
+        return redirect('/login')
+        
     # Retrieve all blog posts from MongoDB
     blogs = mongo.db.blogs.find()
     return render_template("blogs.html", blogs=blogs,userName=currentuser.get('userName'),logined=logined )
@@ -76,7 +76,7 @@ def login():
 def register():
     global logined, currentuser
     msg = ''
-    collection = mongo.db.Users
+    collection = mongo.db.Users       
     if request.method == 'POST':
         userName = request.form['username']
         password = request.form['password']
@@ -94,6 +94,40 @@ def logout():
     logined = False
     currentuser = None
     return redirect('/login')
+
+@app.route("/Search",methods=['GET', 'POST'])
+def search():
+    if not logined:
+        return redirect('/login')
+        
+    if request.method == 'POST' and logined:
+        query = request.form['query']
+        blogs = mongo.db.blogs.find({ '$text': { '$search': query } })
+        blogs1 = mongo.db.blogs.find({ 'author': { '$regex': query, '$options': 'i' } })
+        msg = f'Search reasult for :{query}'
+        return render_template("blogs.html", blogs=list(blogs)+list(blogs1),userName=currentuser.get('userName'),logined=logined ,msg=msg)
+    return render_template("blogs.html", blogs='blogs')
+
+@app.route('/read_more',methods=['GET','POST'])
+def read_more():
+    if not logined:
+        return redirect('/login')
+        
+    if request.method == 'POST':
+        # Assuming you're passing the blog ID as a form parameter named 'blog_id'
+        blog_id = request.form.get('blog_id')
+        print(blog_id)
+        # Retrieve the specific blog post from MongoDB using its ID
+        blog = mongo.db.blogs.find_one({'_id': ObjectId(blog_id)})
+        if blog:
+            # Render the read more template with the specific blog post
+            return render_template("read_more.html", blog=blog, userName=currentuser.get('userName'), logined=logined)
+        else:
+            # If the blog post is not found, you can handle it appropriately
+            return "Blog post not found."
+    else:
+        # If the request method is not POST, handle it accordingly
+        return redirect('/blogs')
     
 if __name__ == '__main__':
     app.run(debug=True)
